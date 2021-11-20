6e59b9d085722a8a3cd7bc98d38c237250f4a943
zkerriga <daniil.63@yandex.ru>
2021-11-21 01:42:51 +0300

    Add rounding to numbers

--- backend/src/main/scala/ru/aboba/backend/types/package.scala
+++ backend/src/main/scala/ru/aboba/backend/types/package.scala
@@ -2,22 +2,27 @@ package ru.aboba.backend
 
 import derevo.circe.{decoder, encoder}
 import derevo.derive
+import io.circe.Encoder
 import org.http4s.QueryParamDecoder
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
 import supertagged.TaggedType
 
 import java.time.LocalDateTime
+import scala.math.BigDecimal.RoundingMode
 
 package object types {
 
-  @derive(decoder, encoder)
+  @derive(decoder)
   object Liter extends TaggedType[BigDecimal] {
     val zero: Liter = Liter(BigDecimal(0))
 
     def from(d: Double): Liter =
       Liter(BigDecimal(d))
 
-    implicit val literDecoder: QueryParamDecoder[Liter] =
+    implicit val literEncoder: Encoder[Liter] =
+      Encoder.encodeBigDecimal.contramap(l => l.setScale(2, RoundingMode.HALF_UP))
+
+    implicit val literQueryParamDecoder: QueryParamDecoder[Liter] =
       QueryParamDecoder[Double].map(Liter.from)
 
     object LiterParamMatcher extends QueryParamDecoderMatcher[Liter]("goalLiters")
@@ -28,12 +33,15 @@ package object types {
   }
   type Liter = Liter.Type
 
-  @derive(decoder, encoder)
+  @derive(decoder)
   object Power extends TaggedType[BigDecimal] {
     val zero: Power = Power(BigDecimal(0))
 
     def from(d: Double): Power =
       Power(BigDecimal(d))
+
+    implicit val powerEncoder: Encoder[Power] =
+      Encoder.encodeBigDecimal.contramap(p => p.setScale(2, RoundingMode.HALF_UP))
 
     implicit class Ops(private val power: Power) extends AnyVal {
       def plus(other: Power): Power = Power(power + other)
@@ -53,10 +61,13 @@ package object types {
   object Timestamp extends TaggedType[LocalDateTime]
   type Timestamp = Timestamp.Type
 
-  @derive(decoder, encoder)
+  @derive(decoder)
   object ConsumptionStatus extends TaggedType[BigDecimal] {
     val best: ConsumptionStatus  = ConsumptionStatus(BigDecimal(1))
     val worst: ConsumptionStatus = ConsumptionStatus(BigDecimal(0))
+
+    implicit val consumptionStatusEncoder: Encoder[ConsumptionStatus] =
+      Encoder.encodeBigDecimal.contramap(c => c.setScale(2, RoundingMode.HALF_UP))
 
     implicit class Ops(private val coefficient: ConsumptionStatus) extends AnyVal {
       def normalize: ConsumptionStatus = {
